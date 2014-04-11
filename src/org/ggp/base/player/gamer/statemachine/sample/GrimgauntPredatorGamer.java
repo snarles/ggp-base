9a5070c7894ce04405f1454cfae24628d16bf2d1
George Elkins <george.f.elkins@gmail.com>
2014-04-10 20:31:13 -0400

    First attempt at a base class.

---
+++ src/org/ggp/base/player/gamer/statemachine/sample/GrimgauntPredatorGamer.java
@@ -0,0 +1,57 @@
+package org.ggp.base.player.gamer.statemachine.sample;
+
+import org.ggp.base.apps.player.detail.DetailPanel;
+import org.ggp.base.apps.player.detail.SimpleDetailPanel;
+import org.ggp.base.player.gamer.exception.GamePreviewException;
+import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
+import org.ggp.base.util.game.Game;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.cache.CachedStateMachine;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
+
+/**
+ * Base class for Grimgaunt Predator.  Extend this abstract class and implement the following methods:
+ *		stateMachineSelectMove
+ *		stateMachineMetaGame (optional)
+ * For example, a stateless exhaustive search would only implement stateMachineSelectMove, with appropriate class name:
+ *		public class GrimgauntPredatorStatelessBritishMuseumGamer extends GrimgauntPredatorGamer {
+ *			@override public Move stateMachineSelectMove(final long timeout) { Exhaustive search code... }
+ *		}
+ */
+public abstract class GrimgauntPredatorGamer extends StateMachineGamer {
+
+	/**
+	 * This is the only part we know, so it's final.
+	 *
+	 * @return String
+	 */
+	@Override
+	public final String getName() {
+		return "Grimgaunt Predator";
+	}
+
+	@Override
+	public DetailPanel getDetailPanel() {
+		return new SimpleDetailPanel();
+	}
+
+	@Override
+	public StateMachine getInitialStateMachine() {
+		return new CachedStateMachine(new ProverStateMachine());
+	}
+
+	@Override
+	public void stateMachineStop() {
+		// Do nothing
+	}
+
+	@Override
+	public void stateMachineAbort() {
+		// Do nothing
+	}
+
+	@Override
+	public void preview(Game g, long timeout) throws GamePreviewException {
+		// Do nothing
+	}
+}
