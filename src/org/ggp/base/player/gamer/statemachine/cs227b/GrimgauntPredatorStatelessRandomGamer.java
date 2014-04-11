eb996dccd9e2138409247a7e0bc6bcd51307468d
George Elkins <george.f.elkins@gmail.com>
2014-04-11 10:56:58 -0400

    Initial working attempt at a new gamer.

---
+++ src/org/ggp/base/player/gamer/statemachine/cs227b/GrimgauntPredatorStatelessRandomGamer.java
@@ -0,0 +1,33 @@
+package org.ggp.base.player.gamer.statemachine.cs227b;
+
+import java.util.List;
+import java.util.Random;
+
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+
+/**
+ * Simplest possible GrimgauntPredatorGamer.  Selects a random legal move.
+ */
+public class GrimgauntPredatorStatelessRandomGamer extends GrimgauntPredatorGamer {
+
+	@Override
+	public String getName() {
+		return super.getName() + " (StatelessRandom)";
+	}
+
+	@Override
+	public void stateMachineMetaGame(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		// Do nothing
+	}
+
+	@Override
+	public Move stateMachineSelectMove(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		final List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
+		return moves.get(new Random().nextInt(moves.size()));
+	}
+}
