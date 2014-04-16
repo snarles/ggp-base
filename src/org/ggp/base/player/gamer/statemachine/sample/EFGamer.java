7ade24e2a4bb33b9f769d0e7c4b3202a5fcb7925
Charles Yang Zheng <snarles@seqmac-16.local>
2014-04-15 21:59:29 -0700

    ef gamer

--- src/org/ggp/base/player/gamer/statemachine/sample/EFGamer.java
+++ src/org/ggp/base/player/gamer/statemachine/sample/EFGamer.java
@@ -120,15 +120,18 @@ public final class EFGamer extends StateMachineGamer
 		// We will also continue considering moves for two seconds, in case we can stumble
 		// upon a move which would cause us to win: if we find such a move, we will just
 		// immediately take it.
-		boolean reasonableMoveFound = false;
-		int maxGoal = 0;
+
+		MachineState s = getCurrentState();
+		System.out.println();
+		System.out.print("The current state");
+		System.out.print(s.toString());
+		System.out.print("is worth");
+		System.out.print(evaluateState(s));
+
+		int best = -1000;
 		for(Move moveUnderConsideration : movesInRandomOrder) {
 		    // Check to see if there's time to continue.
 		    if(System.currentTimeMillis() > finishBy) break;
-
-		    // If we've found a reasonable move, only spend at most two seconds trying
-		    // to find a winning move.
-		    if(System.currentTimeMillis() > start + 2000 && reasonableMoveFound) break;
 
 		    // Get the next state of the game, if we take the move we're considering.
 		    // Since it's our turn, in an alternating-play game the opponent will only
@@ -138,63 +141,20 @@ public final class EFGamer extends StateMachineGamer
 		    // may have many moves, and so we will randomly pick one of our opponent's
 		    // possible actions and assume they do that.
 		    MachineState nextState = theMachine.getNextState(getCurrentState(), theMachine.getRandomJointMove(getCurrentState(), getRole(), moveUnderConsideration));
+		    int value = evaluateState(nextState);
 
-		    // Does the move under consideration end the game? If it does, do we win
-		    // or lose? If we lose, don't bother considering it. If we win, then we
-		    // definitely want to take this move. If its goal is better than our current
-		    // best goal, go ahead and tentatively select it
-		    if(theMachine.isTerminal(nextState)) {
-		        if(theMachine.getGoal(nextState, getRole()) == 0) {
-		            continue;
-		        } else if(theMachine.getGoal(nextState, getRole()) == 100) {
-	                selection = moveUnderConsideration;
-	                break;
-		        } else {
-		        	if (theMachine.getGoal(nextState, getRole()) > maxGoal)
-		        	{
-		        		selection = moveUnderConsideration;
-		        		maxGoal = theMachine.getGoal(nextState, getRole());
-		        	}
-		        	continue;
-		        }
-		    }
+		    System.out.println();
+			System.out.print("The state");
+			System.out.print(nextState.toString());
+			System.out.print("is worth");
+			System.out.print(value);
 
-		    // Check whether any of the legal joint moves from this state lead to
-		    // a loss for us. Again, this only makes sense in the context of an alternating
-		    // play zero-sum game, in which this is the opponent's move and they are trying
-		    // to make us lose, and so if they are offered any move that will make us lose
-		    // they will take it.
-		    boolean forcedLoss = false;
-		    for(List<Move> jointMove : theMachine.getLegalJointMoves(nextState)) {
-		        MachineState nextNextState = theMachine.getNextState(nextState, jointMove);
-		        if(theMachine.isTerminal(nextNextState)) {
-		            if(theMachine.getGoal(nextNextState, getRole()) == 0) {
-		                forcedLoss = true;
-		                break;
-		            }
-		        }
-
-		        // Check to see if there's time to continue.
-		        if(System.currentTimeMillis() > finishBy) {
-		            forcedLoss = true;
-		            break;
-		        }
-		    }
-
-		    // If we've verified that this move isn't going to lead us to a state where
-		    // our opponent can defeat us in one move, we should keep track of it.
-		    if(!forcedLoss) {
-		        selection = moveUnderConsideration;
-		        reasonableMoveFound = true;
+		    if (value > best) {
+		    	best = value;
+		    	selection=moveUnderConsideration;
 		    }
 		}
 
-		//diagnostic
-		MachineState s = getCurrentState();
-		System.out.println("The current state");
-		System.out.println(s.toString());
-		System.out.println("is worth");
-		System.out.println(evaluateState(s));
 
 		long stop = System.currentTimeMillis();
 
