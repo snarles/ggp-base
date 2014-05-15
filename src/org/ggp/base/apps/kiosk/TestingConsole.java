5d34776177e44d2f18b0f86656ea15d87986c237
Charles Yang Zheng <snarles@seqmac-16.stanford.edu>
2014-05-14 19:36:39 -0700

    working on propnet

--- src/org/ggp/base/apps/kiosk/TestingConsole.java
+++ src/org/ggp/base/apps/kiosk/TestingConsole.java
@@ -9,15 +9,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 import org.ggp.base.player.gamer.exception.MetaGamingException;
 import org.ggp.base.player.gamer.exception.MoveSelectionException;
-import org.ggp.base.player.gamer.statemachine.cs227b.MCTSGamer;
+import org.ggp.base.player.gamer.statemachine.sample.SampleMonteCarloGamer;
 import org.ggp.base.util.game.Game;
 import org.ggp.base.util.gdl.grammar.Gdl;
 import org.ggp.base.util.gdl.grammar.GdlConstant;
 import org.ggp.base.util.gdl.grammar.GdlTerm;
 import org.ggp.base.util.match.Match;
+import org.ggp.base.util.propnet.architecture.Component;
+import org.ggp.base.util.propnet.architecture.PropNet;
+import org.ggp.base.util.propnet.architecture.components.Proposition;
+import org.ggp.base.util.propnet.factory.OptimizingPropNetFactory;
 import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Move;
 import org.ggp.base.util.statemachine.StateMachine;
@@ -27,11 +32,12 @@ import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
 
 
 public class TestingConsole {
+	int diaglevel = 10;
 	//Change this:
 	//String dir = "C:/github/ggp-base/games/gamemaster/";
 	String dir = "/Users/snarles/github/ggp-base/games/gamemaster/";
-	String gamef = "alquerque.kif";
-	//String gamef = "connectfour.kif";
+	//String gamef = "alquerque.kif";
+	String gamef = "connectfour.kif";
 	//String gamef = "pentago.kif";
 	//String gamef = "skirmish.kif";
 
@@ -41,13 +47,57 @@ public class TestingConsole {
 	MachineState currentState;
 	int currentTurn = -1;
 	boolean messageEachTurn = true;
+	PropNet pn = null;
 
 	public static void main(String[] args) throws IOException, MoveDefinitionException, FileNotFoundException, TransitionDefinitionException, MetaGamingException, MoveSelectionException {
 		TestingConsole tc = new TestingConsole();
 		tc.run();
 	}
 
-	public void run() throws IOException, MoveDefinitionException, FileNotFoundException, TransitionDefinitionException, MetaGamingException, MoveSelectionException {
+	public void run() {
+		String content = "";
+		try {
+			Scanner scanman = new Scanner(new File(gameFile));
+			content = scanman.useDelimiter("\\Z").next();
+			scanman.close();
+		}
+		catch (IOException e) {
+		}
+		//System.out.println(content);
+		String content2 = Game.preprocessRulesheet(content);
+		System.out.println(content2);
+		Game theGame = Game.createEphemeralGame(content2);
+		List<Gdl> theRules = theGame.getRules();
+		psm.initialize(theRules);
+
+
+//		PropNetAnnotatedFlattener af = new PropNetAnnotatedFlattener(theRules);
+//		long start = System.currentTimeMillis();
+//		List<GdlRule> flatDescription = af.flatten();
+//		long elapsed = System.currentTimeMillis()-start;
+//		printd("Time to flatten",String.valueOf(elapsed),3);
+
+		/**
+		paused(1);
+		for (GdlRule g : flatDescription) {
+			System.out.println(g.toString());
+		}
+		**/
+		//paused(1);
+		OptimizingPropNetFactory op = new OptimizingPropNetFactory();
+		try {
+			pn = op.create(theRules,true);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		paused(1);
+		printComponents();
+		paused(1);
+		printPropositions();
+	}
+
+	public void run3() throws IOException, MoveDefinitionException, FileNotFoundException, TransitionDefinitionException, MetaGamingException, MoveSelectionException {
 		Scanner scanman = new Scanner(new File(gameFile));
 		String content = scanman.useDelimiter("\\Z").next();
 		scanman.close();
@@ -59,10 +109,10 @@ public class TestingConsole {
 		psm.initialize(theRules);
 
 		Match theMatch = new Match("test",-1,10,10,theGame);
-		MCTSGamer sean = new MCTSGamer();
-		sean.setName("sean");
-		MCTSGamer charles = new MCTSGamer();
-		charles.setName("charles");
+		SampleMonteCarloGamer sean = new SampleMonteCarloGamer();
+		//sean.setName("sean");
+		SampleMonteCarloGamer charles = new SampleMonteCarloGamer();
+		//charles.setName("charles");
 
 		GdlConstant r = psm.getRoles().get(0).getName();
 		GdlConstant r2 = psm.getRoles().get(1).getName();
@@ -87,7 +137,7 @@ public class TestingConsole {
 			GdlTerm move1 = sean.selectMove(start + timelimit);
 			System.out.println(String.valueOf(System.currentTimeMillis() - start));
 			if (pauser) {
-				pause();
+				paused(1);
 			}
 			start = System.currentTimeMillis();
 			GdlTerm move2 = charles.selectMove(start+ timelimit);
@@ -95,7 +145,7 @@ public class TestingConsole {
 			MachineState state = sean.getCurrentState();
 			System.out.println(state.toString());
 			if (pauser) {
-				pause();
+				paused(1);
 			}
 			gameOver = psm.isTerminal(state);
 			//Move move1 = psm.getMoveFromTerm(sean.selectMove(System.currentTimeMillis() + 10000));
@@ -166,6 +216,29 @@ public class TestingConsole {
 		}
 	}
 
+
+	public void printComponents() {
+		Set<Component> components = pn.getComponents();
+		for (Component c : components) {
+			printd("Component:",c.toString(),2);
+		}
+	}
+
+	public void printPropositions() {
+		Set<Proposition> propositions = pn.getPropositions();
+		int count = 0;
+		for (Proposition p : propositions) {
+			count++;
+			printd("Proposition:",p.toString(),2);
+			if (count > 1000) {
+				count = 0;
+				paused(2);
+			}
+		}
+	}
+
+
+
 	// chooses a legal joint move at random and advances the game
 	public void randomAdvance() throws TransitionDefinitionException, MoveDefinitionException {
 		long start; long elapsed; String message;
@@ -204,7 +277,11 @@ public class TestingConsole {
 		}
 		return moves;
 	}
-
+	public void printd(String s, String u, int i) {
+		if (i < diaglevel) {
+			System.out.println(s.concat(u));
+		}
+	}
 	public MachineState getNextState(List<Move> move) throws TransitionDefinitionException {
 		long start = System.currentTimeMillis();
 		MachineState newState = psm.getNextState(currentState,move);
@@ -217,14 +294,16 @@ public class TestingConsole {
 		return newState;
 	}
 
-	public void pause() {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("[PAUSED]");
-		try {
-			String gameFile = in.readLine();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public void paused(int i) {
+		if (i < diaglevel) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("[PAUSED]");
+			try {
+				String gameFile = in.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
